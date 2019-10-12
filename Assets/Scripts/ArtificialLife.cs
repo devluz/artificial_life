ccc2fc391ec1c1384d66e5e58eddaee0e7dab85e
Christoph <contact@because-why-not.com>
2019-10-12 18:16:50 +1300

    Added mutation & selection + the simulation can now be speed up + lots of other small changes * Color changes based on speed (red) and view range (blue) * using Physics.Simulate to run multiple simulation stops during a single frame. if the FPS drop below 20 it will manually slow down * Use World._TimeFactor to change the speed * Als that get pushed of the area are now put into napping state to avoid extending the round

--- Assets/Scripts/ArtificialLife.cs
+++ Assets/Scripts/ArtificialLife.cs
@@ -5,28 +5,57 @@ using UnityEngine;
 
 public class ArtificialLife : MonoBehaviour
 {
-    public float _MovementSpeed = 1;
-    public float _ViewDistance = 5;
-    
+    private static bool VERBOSE_LOG = false;
+
+    [Serializable]
+    public class Properties
+    {
+        public readonly static float MOVEMENT_SPEED_MAX = 20;
+        public readonly static float VIEW_DISTANCE_MAX = 20;
+        public float _MovementSpeed = 5;
+        public float _ViewDistance = 5;
+
+        public Properties(float speed, float view)
+        {
+            _MovementSpeed = speed;
+            _ViewDistance = view;
+        }
+
+        public Properties()
+        {
+
+        }
+    }
+    public Properties properties = new Properties();
     private Rigidbody mRb;
 
 
     private int mEaten = 0;
+    public int Eaten
+    {
+        get
+        {
+            return mEaten;
+        }
+    }
 
-    enum State
+    public enum AlState
     {
         LookingForFood,
         MovingToFood,
         Napping
     }
 
-    private State mState = State.LookingForFood;
+    private AlState mState = AlState.LookingForFood;
+    public AlState State
+    {
+        get
+        {
+            return mState;
+        }
+    }
 
-    /// <summary>
-    /// Counts the physics steps used for easy timing of regular tasks
-    /// </summary>
-    private int mStep = 0;
-    private readonly int STEPS_PER_SEC = 50;
+
     private Vector3 mRandomDir = new Vector3(0,0,0);
 
 
@@ -35,6 +64,24 @@ public class ArtificialLife : MonoBehaviour
     private void Start()
     {
         mRb = GetComponent<Rigidbody>();
+    }
+
+    public void SetProperties(Properties props)
+    {
+        properties = props;
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        var renderer = this.gameObject.GetComponent<MeshRenderer>();
+
+        float speed = properties._MovementSpeed / Properties.MOVEMENT_SPEED_MAX;
+        float view = properties._ViewDistance / Properties.VIEW_DISTANCE_MAX;
+
+
+
+        renderer.material.color = new Color(speed, 0.5f, view);
     }
 
     private void Update()
@@ -61,26 +108,26 @@ public class ArtificialLife : MonoBehaviour
     {
         Vector3 curPos = mRb.position;
 
-        Vector3 movePos = curPos + dir * _MovementSpeed * Time.fixedDeltaTime;
+        Vector3 movePos = curPos + dir * properties._MovementSpeed * Time.fixedDeltaTime;
         
         mRb.MovePosition(movePos);
     }
 
-    private void FixedUpdate()
+    public void SimulationStep(int step)
     {
-        if(mStep % STEPS_PER_SEC == 0)
+        if(step % Simulation.STEPS_PER_SEC == 0)
         {
             mRandomDir = GetRandomDir();
         }
 
 
 
-        if(mState == State.LookingForFood)
+        if(mState == AlState.LookingForFood)
         {
             Move(mRandomDir);
 
             var pos = this.transform.position;
-            Collider[] res = Physics.OverlapSphere(this.mRb.position, 20, Food.GetLayerMask());
+            Collider[] res = Physics.OverlapSphere(this.mRb.position, properties._ViewDistance, Food.GetLayerMask());
             Array.Sort(res, (Collider a, Collider b) => {
                 float dista = Vector3.Distance(this.mRb.position, a.transform.position);
                 float distb = Vector3.Distance(this.mRb.position, b.transform.position);
@@ -93,15 +140,16 @@ public class ArtificialLife : MonoBehaviour
             });
             if(res != null && res.Length > 0)
             {
-                mState = State.MovingToFood;
+                mState = AlState.MovingToFood;
                 mTarget = res[0].GetComponent<Food>();
                 float distance = Vector3.Distance(mTarget.transform.position, this.mRb.position);
-                Debug.Log(this.mRb.position + " found food at " + mTarget.transform.position + "  " + distance);
+                if(VERBOSE_LOG)
+                    Debug.Log(this.mRb.position + " found food at " + mTarget.transform.position + "  " + distance);
             }
 
 
         }
-        else if(mState == State.MovingToFood)
+        else if(mState == AlState.MovingToFood)
         {
             if(mTarget != null)
             {
@@ -112,11 +160,16 @@ public class ArtificialLife : MonoBehaviour
             }
             else
             {
-                mState = State.LookingForFood;
+                mState = AlState.LookingForFood;
             }
         }
 
-        mStep++;
+        //special case. due to some troubles in the physics they can be pushed over the edge
+        //or through it -> switch them to nap time
+        if(this.mRb.position.y < -10)
+        {
+            this.mState = AlState.Napping;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -130,13 +183,14 @@ public class ArtificialLife : MonoBehaviour
             {
                 if(mEaten < 2)
                 {
-                    Debug.Log(this.gameObject.name + " ate " + f.name);
+                    if(VERBOSE_LOG)
+                        Debug.Log(this.gameObject.name + " ate " + f.name);
                     Destroy(f.gameObject);
                     mEaten++;
 
                     if(mEaten == 2)
                     {
-                        mState = State.Napping;
+                        mState = AlState.Napping;
                     }
                 }
             }
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,14 @@ public class Simulation : MonoBehaviour
 {
     public static readonly float RADIUS = 20;
 
+    public enum SimState
+    {
+        NotYetStarted,
+        InRound,
+        InBetweenRounds,
+        Ended
+    }
+    private SimState mState = SimState.NotYetStarted;
 
     public int _NumberOfFood = 20;
     public int _NumberOfLife = 4;
@@ -16,16 +25,114 @@ public class Simulation : MonoBehaviour
     public GameObject _LifePrefab;
     public GameObject _FoodPrefab;
 
+    public readonly static int STEPS_PER_SEC = 50;
+    private int mStep = 0;
+
+    List<ArtificialLife.Properties> mNextGen = null;
+
+    private int mRound = 0;
+
+    public float _TimeFactor = 1;
+
+
+    public float _BreakBeforeRound = 0.1f;
+    public float _BreakAfterRound = 2;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        mNextGen = new List<ArtificialLife.Properties>();
+        mNextGen.Add(new ArtificialLife.Properties(2, 2));
+        mNextGen.Add(new ArtificialLife.Properties(2, 2));
+        mNextGen.Add(new ArtificialLife.Properties(2, 2));
+        mNextGen.Add(new ArtificialLife.Properties(2, 2));
+
+        Physics.autoSimulation = false;
         MakeBarrier();
-        SpawnFood();
-        SpawnLife();
-        //needed otherwise rigidbody.position isn't updated yet to the position we placed the objects!
-        Physics.SyncTransforms();
+        PrepareNextRound();
+        StartRound();
+    }
+    private float mPhysicsTimer;
+    void Update()
+    {
+        if (_TimeFactor < 0)
+            _TimeFactor = 0;
+
+        if (_TimeFactor == 0)
+            return;
+
+        DateTime start = DateTime.Now;
+
+        if (mState == SimState.InRound)
+        {
+            mPhysicsTimer += Time.deltaTime * _TimeFactor;
+
+            // Catch up with the game time.
+            // Advance the physics simulation in portions of Time.fixedDeltaTime
+            // Note that generally, we don't want to pass variable delta to Simulate as that leads to unstable results.
+            while (mPhysicsTimer >= Time.fixedDeltaTime)
+            {
+                mPhysicsTimer -= Time.fixedDeltaTime;
+                SimulationStep();
+                Physics.SyncTransforms();
+                Physics.Simulate(Time.fixedDeltaTime);
+
+                //skip if we take so long that the FPS drop below 20
+                //and skip if the round ended anyway
+                if( (DateTime.Now - start).TotalSeconds  > 0.05 || mState != SimState.InRound)
+                {
+                    mPhysicsTimer = 0;
+                    break;
+                }
+            }
+        }
     }
 
+    private void SimulationStep()
+    {
+        ArtificialLife[] als = GetComponentsInChildren<ArtificialLife>();
+        foreach(var v in als)
+        {
+            v.SimulationStep(mStep);
+        }
+        if (mStep % STEPS_PER_SEC == 1) 
+        {
+            Food[] foods = GetComponentsInChildren<Food>();
+            if(foods.Length == 0)
+            {
+                Debug.Log("Ending round. Everything is eaten");
+                EndRound();
+                return;
+            }
+
+            int napping = 0;
+            foreach(var v in als)
+            {
+                if(v.State == ArtificialLife.AlState.Napping)
+                {
+                    napping++;
+                }
+            }
+
+            if(als.Length == napping) {
+
+                Debug.Log("Ending round. Everyone is napping.");
+                EndRound();
+                return;
+            }
+
+
+            if(mStep > STEPS_PER_SEC * 60)
+            {
+                Debug.Log("Ending round due to timeout.");
+                EndRound();
+                return;
+            }
+        }
+        mStep++;
+    }
 
     private void MakeBarrier()
     {
@@ -55,7 +162,7 @@ public class Simulation : MonoBehaviour
     {
         for(int i = 0; i < _NumberOfFood; i++)
         {
-            Vector2 pos = Random.insideUnitCircle * (RADIUS - 2);
+            Vector2 pos = UnityEngine.Random.insideUnitCircle * (RADIUS - 2);
 
             GameObject food = Instantiate(_FoodPrefab);
             food.transform.position = new Vector3(pos.x, 2, pos.y);
@@ -66,9 +173,14 @@ public class Simulation : MonoBehaviour
 
     private void SpawnLife()
     {
-        float circ = Mathf.PI * 2 * RADIUS;
-        float angle_step = Mathf.PI * 2 / _NumberOfLife;
-        for (int i = 0; i < _NumberOfLife; i++)
+        int num = _NumberOfLife;
+        if (mNextGen != null)
+        {
+            num = mNextGen.Count;
+        }
+        float angle_step = Mathf.PI * 2 / num;
+
+        for (int i = 0; i < num; i++)
         {
             float own_angle = angle_step * i;
 
@@ -84,12 +196,108 @@ public class Simulation : MonoBehaviour
             life.transform.position = pos;
             life.transform.parent = this.transform;
             life.transform.name = "Life_" + i;
+            //if we already have population info then
+            //override the defaults
+            if(mNextGen != null)
+            {
+                life.GetComponent<ArtificialLife>().SetProperties(mNextGen[i]);
+
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void PrepareNextRound()
     {
-        
+        SpawnFood();
+        SpawnLife();
+        //needed otherwise rigidbody.position isn't updated yet to the position we placed the objects!
+        Physics.SyncTransforms();
     }
+    private void StartRound()
+    {
+        mRound++;
+        Debug.Log("Start round " + mRound);
+        mStep = 0;
+        mState = SimState.InRound;
+    }
+
+    /// <summary>
+    /// Ends the round and does all the mutating for the next round.
+    /// 
+    /// </summary>
+    private void EndRound()
+    {
+        mState = SimState.InBetweenRounds;
+        mNextGen = new List<ArtificialLife.Properties>();
+
+
+        ArtificialLife[] als = GetComponentsInChildren<ArtificialLife>();
+        foreach (var v in als)
+        {
+            if(v.Eaten == 1)
+            {
+                mNextGen.Add(v.properties);
+            }else if (v.Eaten >= 2)
+            {
+                mNextGen.Add(v.properties);
+                mNextGen.Add(Mutate(v.properties));
+            }
+        }
+
+        if (mNextGen.Count == 0)
+        {
+            Debug.LogWarning("They all died! :(");
+            mState = SimState.Ended;
+        }
+        else
+        {
+            StartCoroutine(CoroutinePrepareRound());
+        }
+    }
+
+    private void Cleanup()
+    {
+        Food[] foods = GetComponentsInChildren<Food>();
+        foreach (var v in foods)
+        {
+            Destroy(v.gameObject);
+        }
+        ArtificialLife[] als = GetComponentsInChildren<ArtificialLife>();
+        foreach (var v in als)
+        {
+            Destroy(v.gameObject);
+        }
+        Physics.SyncTransforms();
+    }
+
+    ArtificialLife.Properties Mutate(ArtificialLife.Properties props)
+    {
+
+        ArtificialLife.Properties res = new ArtificialLife.Properties();
+        res._MovementSpeed = props._MovementSpeed + (UnityEngine.Random.value - 0.5f) * 2.0f;
+        res._ViewDistance = props._ViewDistance + (UnityEngine.Random.value - 0.5f) * 2.0f;
+
+        return res;
+    }
+
+
+
+    private IEnumerator CoroutinePrepareRound()
+    {
+        //pause before cleaning up possily allowing the user
+        //to inspect what happened in this round
+        yield return new WaitForSecondsRealtime(_BreakBeforeRound);
+        //removing all left overs of the last round
+        Cleanup();
+        //setup map for the next round
+        PrepareNextRound();
+
+        //again wait a little to allow the user to see the new
+        //life 
+        yield return new WaitForSecondsRealtime(_BreakAfterRound);
+        StartRound();
+    }
+
 }
+
